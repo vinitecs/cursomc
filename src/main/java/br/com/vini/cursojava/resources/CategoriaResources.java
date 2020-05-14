12b224dc7fdcfc940705e8f87e31b31ec8000cba
vinicius lopes <viniciusamalia@gmail.com>
2020-05-14 18:53:10 -0300

    testando a primeira classe de de dominio - categoria

--- src/main/java/br/com/vini/cursojava/resources/CategoriaResources.java
+++ src/main/java/br/com/vini/cursojava/resources/CategoriaResources.java
@@ -1,14 +1,29 @@
 package br.com.vini.cursojava.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.vini.cursojava.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
+	
 		@RequestMapping(method=RequestMethod.GET)
-		public String listar() {
-			return "rest está funcionando!!! poura";
+		public List<Categoria> listar() {
+			
+			Categoria cat1 = new Categoria(1, "informatica");
+			Categoria cat2 = new Categoria(2,"escritorio");
+			
+			List<Categoria> lista =  new ArrayList<>();
+			lista.add(cat1);
+			lista.add(cat2);
+			
+			return lista;
 		}
+		
 }
